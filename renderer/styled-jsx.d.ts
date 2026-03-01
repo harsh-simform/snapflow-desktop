25feb278cae28c2db1e303b4fff7a771f57671ad
Harsh Makwana <harsh.make1998@gmail.com>
2026-03-01 14:29:50 +0530

    refactor(styled-jsx): simplify JSX intrinsic elements definition

--- renderer/styled-jsx.d.ts
+++ renderer/styled-jsx.d.ts
@@ -1,23 +1,11 @@
-/* eslint-disable no-undef */
-
-import type React from "react";
-
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      style: React.DetailedHTMLProps<
-        {
-          jsx?: boolean;
-          global?: boolean;
-          dynamic?: string | string[];
-          [key: string]: any;
-        } & React.StyleHTMLAttributes<HTMLStyleElement>,
-        HTMLStyleElement
-      > & {
-        children?: string;
-      };
-    }
+declare namespace JSX {
+  interface IntrinsicElements {
+    style: {
+      jsx?: boolean | undefined;
+      global?: boolean | undefined;
+      dynamic?: string | string[] | undefined;
+      children?: string | undefined;
+      [key: string]: any;
+    };
   }
 }
-
-export {};
